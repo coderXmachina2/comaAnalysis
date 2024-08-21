445892c5e30b22e77e1474117feb3548fcc97978
coderXmachina2 <technologeeks92@gmail.com>
2024-08-21 11:12:25 +0800

    testThing

--- comaPyFuncs/paramsCutsJoins.py
+++ comaPyFuncs/paramsCutsJoins.py
@@ -3,3 +3,6 @@ joins = [('top_left_diagonal','bottom_right_diagonal'),
          ('left','right'),
          ('bottom_left_diagonal', 'top_right_diagonal'),
          ('down', 'up')]
+
+
+print("This will be deleted!")
